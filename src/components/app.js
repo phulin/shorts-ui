37454a1ba4426ea3be9b33940c56953ce3790395
Patrick Hulin <patrick.hulin@gmail.com>
2020-09-15 19:16:55 -0400

    Add chess pieces.

--- src/components/app.js
+++ src/components/app.js
@@ -38,6 +38,21 @@ const itemPockets = [
 	['Tangerine', 'Hawking\'s elixir', 396, '/images/itemimages/tangerine.gif'],
 ];
 
+const chessPockets = [
+	['Onyx pawn', '', 105, '/images/itemimages/chess7.gif'],
+	['Onyx knight', '', 4, '/images/itemimages/chess12.gif'],
+	['Onyx bishop', '', 537, '/images/itemimages/chess8.gif'],
+	['Onyx rook', '', 197, '/images/itemimages/chess9.gif'],
+	['Onyx queen', '', 506, '/images/itemimages/chess11.gif'],
+	['Onyx king', '', 88, '/images/itemimages/chess10.gif'],
+	['Alabaster pawn', '', 46, '/images/itemimages/chess1.gif'],
+	['Alabaster knight', '', 275, '/images/itemimages/chess6.gif'],
+	['Alabaster bishop', '', 567, '/images/itemimages/chess2.gif'],
+	['Alabaster rook', '', 111, '/images/itemimages/chess3.gif'],
+	['Alabaster queen', '', 651, '/images/itemimages/chess5.gif'],
+	['Alabaster king', '', 523, '/images/itemimages/chess4.gif'],
+];
+
 const App = () => (
 	<div id="preact_root">
 		<ButtonRow title="Stats" buttons={statPockets} />
@@ -45,6 +60,7 @@ const App = () => (
 		<ButtonRow title="Bell Fights" buttons={bellPockets} />
 		<ButtonRow title="Buffs" buttons={buffPockets} />
 		<ButtonRow title="Items" buttons={itemPockets} />
+		<ButtonRow title="Chess Pieces" buttons={chessPockets} />
 	</div>
 )
 
